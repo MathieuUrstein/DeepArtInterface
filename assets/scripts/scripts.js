ea3c3d0782fa9cce9eaa5f66564a49bc92d4278b
Mathieu Urstein <mathieu.urstein@gmail.com>
2017-01-20 12:57:50 +0100

    Better steps menu

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,30 +1,31 @@
 var video = document.querySelector("#webcam");
 var loading = document.querySelector("#loading-processing");
-var steps = document.querySelectorAll(".step-of-process");
+var stepsProcess = document.querySelectorAll(".step-of-process");
+var stepsGeneral = document.querySelectorAll(".step");
 
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
 
-function handleVideo(stream) {
-   video.src = window.URL.createObjectURL(stream);
-}
-function videoError(e) {
-   console.log(e);
-}
-if (navigator.getUserMedia) {
-   navigator.getUserMedia({video: true}, handleVideo, videoError);
-}
+stepsGeneral.forEach(function(step){
+   step.onmouseover = function(){
+      step.children[0].style.visibility = "visible";
+      step.children[1].style.visibility = "hidden";
+   };
+   step.onmouseout = function(){
+      step.children[0].style.visibility = "hidden";
+      step.children[1].style.visibility = "visible";
+   };
+});
 
 (function waitNextStep(currentStep) {
-   if(currentStep < steps.length - 1) {
-      steps[currentStep].style.display = "block";
-      loading.innerHTML = parseInt(Math.floor((currentStep / steps.length) * 100)) + ' %';
+   if(currentStep < stepsProcess.length - 1) {
+      stepsProcess[currentStep].style.display = "block";
+      loading.innerHTML = parseInt(Math.floor((currentStep / stepsProcess.length) * 100)) + ' %';
       setTimeout(function() {
-         steps[currentStep].style.display = "none";
+         stepsProcess[currentStep].style.display = "none";
          waitNextStep(currentStep+1);
       }, 2000);
-   } else if(currentStep < steps.length) {
-      steps[currentStep].style.display = "block";
-      loading.innerHTML = parseInt(Math.floor((currentStep / steps.length) * 100)) + ' %';
+   } else if(currentStep < stepsProcess.length) {
+      stepsProcess[currentStep].style.display = "block";
+      loading.innerHTML = parseInt(Math.floor((currentStep / stepsProcess.length) * 100)) + ' %';
       setTimeout(function() {
          window.location.href = "step-result.html";
       }, 2000);
